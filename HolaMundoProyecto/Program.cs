acc3cdb6d368848dbd607b3bb3c3b7fe21bdf476
America Almanza Condori <america.almanza@tajamar365.com>
2023-04-18 20:14:19 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundoProyecto/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HolaMundoProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(10 + 10);
+            Console.WriteLine("10" + "10");
+            var nombre = "Juan";
+            Console.WriteLine(nombre);
+
+
+
+            //HolaMundoProyecto.Moneda.prueba();
+        }
+    }
+
+    //public class moneda
+    //{
+    //    static void prueba()
+    //    {
+
+    //    }
+    //}
+}
+
+//ctrl+d copiar linea
+//ctrl+k+c comentar. ctrl+k+u
+//
